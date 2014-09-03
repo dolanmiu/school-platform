e82686f98b5c8570fcbc1e42a9b3c66b9b949f1e
Dolan Miu <dolan_miu@hotmail.com>
2014-09-04 00:56:30 +0100

    Added initial support for GPS
    Created 3 classes. One for the module. One model representing the GPS
    data, and one class modelling the data layer

---
+++ Server/Server/Backend/GPS/GPSData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.GPS
+{
+    public class GPSData : IGPSData
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public interface IGPSData
+    {
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+    }
+}
---
+++ Server/Server/Backend/Modules/GPSModule.cs
@@ -0,0 +1,38 @@
+﻿using Backend.DataLayer;
+using Backend.GPS;
+using Nancy;
+using Nancy.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Backend.Modules
+{
+    public class GPSModule : NancyModule
+    {
+        public GPSModule() 
+        {
+            Get["/gps/{latitude}/{longitude}"] = parameters =>
+            {
+                var data = this.Bind<GPSData>();
+                var gpsData = new GPSData
+                {
+                    Longitude = data.Longitude,
+                    Latitude = data.Latitude
+                };
+
+
+                var dbLayer = new DatabaseLayer();
+                var result = dbLayer.InsertToDatabase("dolan", gpsData);
+
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithContentType("text/html")
+                    .WithModel(result);
+            };
+        }
+
+    }
+}
