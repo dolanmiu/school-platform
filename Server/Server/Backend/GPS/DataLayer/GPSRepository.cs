66e33bbe661448ee1a7567f4b2db2e0618df756f
Dolan Miu <dolan_miu@hotmail.com>
2014-09-05 01:23:50 +0100

    Following Repository design pattern

---
+++ Server/Server/Backend/GPS/DataLayer/GPSRepository.cs
@@ -0,0 +1,46 @@
+﻿using Backend.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.GPS.DataLayer
+{
+    public class GPSRepository : IGPSRepository
+    {
+        //Gateway is a fancy word for the layer just above the database
+        private IGPSGateway gateway;
+        private IGPSDataFactory factory;
+
+        public GPSRepository(IGPSGateway gateway, IGPSDataFactory factory)
+        {
+            if (gateway == null || factory == null)
+            {
+                throw new ArgumentException("Gateway or Factory must not be null");
+            }
+
+            this.gateway = gateway;
+            this.factory = factory;
+        }
+
+        //Returns false if failed, true if successful insert
+        public bool AddGPSData(string userID, GPSData data)
+        {
+            bool isSuccess = gateway.InsertToDatabase(userID, data);
+            return isSuccess;
+        }
+
+        public GPSData GetGPSData(string userID)
+        {
+            string[] rawData = gateway.GetFromDatabase(userID);
+            return factory.Instance(Double.Parse(rawData[0]), Double.Parse(rawData[1]));
+        }
+    }
+
+    public interface IGPSRepository
+    {
+        bool AddGPSData(string userID, GPSData data);
+        GPSData GetGPSData(string userID);
+    }
+
+}
---
+++ Server/Server/Backend/GPS/GPSDataFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.GPS
+{
+    public class GPSDataFactory : IGPSDataFactory
+    {
+        public GPSData Instance(double longitude, double latitude)
+        {
+            var gpsData = new GPSData
+            {
+                Longitude = longitude,
+                Latitude = latitude
+            };
+            return gpsData;
+        }
+    }
+
+    public interface IGPSDataFactory
+    {
+        GPSData Instance(double longitude, double latitude);
+    }
+}
