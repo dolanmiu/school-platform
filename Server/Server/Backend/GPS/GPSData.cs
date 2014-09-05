23e977e2f319119ea714d77f537537f0a5ebc717
Dolan Miu <dolan_miu@hotmail.com>
2014-09-05 01:25:17 +0100

    Removed Interface from GPSData

--- Server/Server/Backend/GPS/GPSData.cs
+++ Server/Server/Backend/GPS/GPSData.cs
@@ -5,15 +5,9 @@ using System.Web;
 
 namespace Backend.GPS
 {
-    public class GPSData : IGPSData
+    public class GPSData
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-    }
-
-    public interface IGPSData
-    {
-        double Latitude { get; set; }
-        double Longitude { get; set; }
     }
 }
