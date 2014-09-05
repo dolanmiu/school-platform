996561cf9e4341c8246e53e69cfc96fe5e4feea5
Dolan Miu <dolan_miu@hotmail.com>
2014-09-05 01:23:21 +0100

    Renamed DatabaseLayer to GPSGateway
    Following design patterns

---
+++ Server/Server/Backend/GPS/DataLayer/GPSGateway.cs
@@ -0,0 +1,28 @@
+﻿using Backend.GPS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.DataLayer
+{
+    public class GPSGateway : IGPSGateway
+    {
+        public bool InsertToDatabase(string userID, GPSData gpsData)
+        {
+            //Add LINQ database logic
+            return true;
+        }
+
+        public string[] GetFromDatabase(string userID)
+        {
+            return new string[] { "234", "344" };
+        }
+    }
+
+    public interface IGPSGateway
+    {
+        bool InsertToDatabase(string userID, GPSData gpsData);
+        string[] GetFromDatabase(string userID);
+    }
+}
