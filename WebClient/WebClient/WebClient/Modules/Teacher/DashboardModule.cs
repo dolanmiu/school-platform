896bb4a012ad5196d2c630adee34dee76318d6ac
Dolan Miu <dolan_miu@hotmail.com>
2014-09-07 17:32:51 +0100

    Added other dashboards

---
+++ WebClient/WebClient/WebClient/Modules/Teacher/DashboardModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClient.Modules.Teacher
+{
+    public class DashboardModule : NancyModule
+    {
+        public DashboardModule() : base("/teacher")
+        {
+
+        }
+    }
+}
