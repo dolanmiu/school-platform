fd9f0d17dcff03bf53c568459684a635762a9474
Dolan Miu <dolan_miu@hotmail.com>
2014-09-07 02:38:00 +0100

    Nancy WebClient

---
+++ WebClient/WebClient/WebClient/Modules/LoginModule.cs
@@ -0,0 +1,42 @@
+﻿using Nancy;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClient.Modules
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule() : base("/login")
+        {
+            Get["/"] = parameters =>
+            {
+                return View["login"];
+            };
+
+            Get["/auth"] = parameters =>
+            {
+                var client = new RestClient("https://euw.api.pvp.net");
+                // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+                var request = new RestRequest("api/lol/euw/v1.2/champion?api_key=e511e298-0729-48e1-9f2c-20639de4741a", Method.GET);
+                //request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
+                //request.AddUrlSegment("id", 123); // replaces matching token in request.Resource
+
+                // easily add HTTP Headers
+                //request.AddHeader("header", "value");
+
+                // add files to upload (works with compatible verbs)
+                //request.AddFile(path);
+
+                // execute the request
+                IRestResponse response = client.Execute(request);
+                var content = response.Content; // raw content as string
+
+                return content;
+            };
+        }
+    }
+}
