061e301b4de77bba400e63f05c9d81efd117bd6a
Dolan Miu <dolan_miu@hotmail.com>
2014-09-08 01:52:23 +0100

    Added routes to student module

---
+++ Server/Server/Backend/Modules/StudentModule.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Modules
+{
+    public class StudentModule : NancyModule
+    {
+        public StudentModule() : base("/student")
+        {
+            Get["/parents/{parentID}"] = parameters =>
+            {
+                return "Should return a list of students the parent gave birth to";
+            };
+
+            Get["/class/{classID}"] = parameters =>
+            {
+                return "Should return a list of students in a particular class";
+            };
+
+            Get["/year/{year}"] = parameters =>
+            {
+                return "Should return a list of students in a particular year";
+            };
+        }
+    }
+}
