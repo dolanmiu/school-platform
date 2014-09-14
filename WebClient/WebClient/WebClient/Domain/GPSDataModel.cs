4565c237acca86826f5bd9825f4f7a11d0010140
Dolan Miu <dolan_miu@hotmail.com>
2014-09-15 00:21:26 +0100

    Revert "Revert "Added GPS functionality""
    This reverts commit c1061a9d7bf0ecf169093d67638e19d6f154775a.

---
+++ WebClient/WebClient/WebClient/Domain/GPSDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClient.Domain
+{
+    public class GPSDataModel
+    {
+        public IList<Coordinate> Coordinates { get; set; }
+    }
+
+    public struct Coordinate {
+        public float x;
+        public float y;
+    }
+}
