92ae655812df61971ddf8fa02e501c2cb96d16c9
Dolan Miu <dolan_miu@hotmail.com>
2014-09-15 00:23:01 +0100

    Changed parents dashboard for gps data and student

--- WebClient/WebClient/WebClient/Modules/Parents/DashboardModule.cs
+++ WebClient/WebClient/WebClient/Modules/Parents/DashboardModule.cs
@@ -3,6 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebClient.Domain;
+using WebClient.Domain.Factories;
+using WebClient.Gateways;
+using WebClient.Repositories;
 
 namespace WebClient.Modules.Parents
 {
@@ -10,9 +14,14 @@ namespace WebClient.Modules.Parents
     {
         public DashboardModule() : base("/parent")
         {
+            IStudentRepository repo = new StudentRepository(new StudentGateway(), new StudentFactory());
+
+
             Get["/"] = parameters =>
             {
-                return View["/Parents/dashboard"];
+
+                IList<StudentModel> model = repo.GetStudents("parentID");
+                return View["/Parents/dashboard", model];
             };
 
             Get["/GetStudents"] = parameters =>
