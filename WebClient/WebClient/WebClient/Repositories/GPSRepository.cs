f5f78ba2362a0c34caf4a334a60e4d84d629ca50
Dolan Miu <dolan_miu@hotmail.com>
2014-09-15 00:22:37 +0100

    GPS repository added

---
+++ WebClient/WebClient/WebClient/Repositories/GPSRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebClient.Domain;
+using WebClient.Domain.Factories;
+using WebClient.Gateways;
+
+namespace WebClient.Repositories
+{
+    public class GPSRepository
+    {
+        private IGPSGateway gateway;
+        private IGPSFactory factory;
+        public GPSRepository(IGPSGateway gateway, IGPSFactory factory)
+        {
+            this.gateway = gateway;
+            this.factory = factory;
+        }
+        public IList<GPSData> GetStudentGPSCoordinates(string studentID, long timestamp)
+        {
+            IList<GPSData> gpsCoords = new List<GPSData>();
+            float[,] coords = this.gateway.GetStudentGPSCoordinates(studentID, timestamp);
+            
+            for (int i = 0; i < coords.GetLength(0); i++)
+            {
+                gpsCoords.Add(factory.Instance(coords[i, 0], coords[i, 1]));
+            }
+            return gpsCoords;
+        }
+    }
+}
