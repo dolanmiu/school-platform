57fce436e8b2c624838b2d1a7da4c3c60b467fc1
Dolan Miu <dolan_miu@hotmail.com>
2014-09-15 01:36:17 +0100

    Added a student profile module

---
+++ WebClient/WebClient/WebClient/Modules/StudentProfileModule.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebClient.Domain;
+using WebClient.Domain.Factories;
+using WebClient.Gateways;
+using WebClient.Repositories;
+
+namespace WebClient.Modules
+{
+    public class StudentProfileModule : NancyModule
+    {
+        public StudentProfileModule() : base("/profile") {
+
+            Get["/{id}"] = parameters =>
+            {
+                IGPSRepository repo = new GPSRepository(new GPSGateway(), new GPSFactory());
+                IList<GPSData> model = repo.GetStudentGPSCoordinates("sdf", 234234);
+                return View["/studentmap", model]; ;
+            };
+        }
+    }
+}
